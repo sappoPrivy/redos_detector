18a51d04e1f89028666e19ae5a6b479d378ddbb4
Silvia Lin Lü <ssll@kth.se>
2026-05-20 15:53:46 +0200

    Found some repositories/code containing regexes

--- web scraping/webscrape.py
+++ web scraping/webscrape.py
@@ -1,46 +1,39 @@
-# Source - https://stackoverflow.com/q/74796978
-# Posted by ladybug
-# Retrieved 2026-05-19, License - CC BY-SA 4.0
-
 import requests
 from bs4 import BeautifulSoup
 import urllib
 from urllib import request
 import urllib.request as ur
+import json
 
 import pandas as pd
 import re
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36'}
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36'}
 
-search_topics = "https://github.com/search?q=web&type=repositories"
+# url = "https://github.com/search?q=regex+language%3APython&type=code"
 # https://github.com/search?q=web+app+language%3APython+language%3AJavaScript&type=repositories&l=JavaScript
-# 
+# https://github.com/search?q=regex+language%3APython&type=code
 
-import requests
-from bs4 import BeautifulSoup
-import json
   
-url = 'https://github.com/psf/requests'
+# url = 'https://github.com/psf/requests'
+
+# python, java?
+url = "https://api.github.com/search/code?q=regex +in:file +language:java"
+
+headers = {
+  'Authorization': 'Token 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'
+}
 
 
-dico = []
+# append the found names here
+results = []
 
-for page in range(1, 99):
+for i in range(1):
+    # get the url content, then render html to access html parts
+    req = requests.get(url, headers=headers).json()
+    # print(req)
 
-    req = requests.get(search_topics + str(page) + "&q=" + "web" + "&type=Repositories", headers = headers)
-    soup = BeautifulSoup(req.text, "html.parser")
-
-    for repo in soup.select('ul.repo-list>li:has(a.v-align-middle[href])'):
-        link = repo.select_one('a.v-align-middle[href]')
-        about = repo.select_one('p.mb-1') 
-
-        dico.append({
-            # 'name': re.sub(r"\/(.*)\/(.*)", "\1", link.get('href')),
-            'name': ' by '.join(link.text.strip().split('/', 1)[::-1]),
-            'url': "https://github.com" + link.get('href'),
-            'about': about.text.strip() if about else None
-        })
-
-df = pd.DataFrame(dico)
-
+    results = [item["repository"]["full_name"] for item in req["items"]]
+    # print(results)
+    with open('regex-repos-java.txt', 'w', encoding='utf-8') as f:
+        json.dump(results, f, ensure_ascii=False)
