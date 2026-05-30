36350d481216a37138e4e52bab768d74d317436a
sappo <sangpo.choedon@gmail.com>
2026-05-30 19:13:05 +0200

    Applied ReScue and Regexploit on 23 Github regexes

--- ReScue code/rescue_run.sh
+++ ReScue code/rescue_run.sh
@@ -5,14 +5,16 @@ cd ../ReScue-master/release
 
 # Define input and output files
 LLM_REGEXES="../../LLM code/LLM_regexes.txt"
-RESULTS="../../ReScue code/rescue_results.txt"
+LLM_RESULTS="../../ReScue code/rescue_llm_results.txt"
+GIT_RESULTS="../../ReScue code/rescue_git_results.txt"
+GIT_REGEXES="../../web scraping/repo-python.txt"
 
 # Ensure the output file is empty
-> "$RESULTS"
+> "$GIT_RESULTS"
 
 # Read each regex and use input to rescue command
 while IFS= read -r regex; do
-    echo "### Testing $regex ###" >> "$RESULTS"
-    echo "$regex" | java -jar ReScue.jar >> "$RESULTS"
-    echo >> "$RESULTS"
-done < "$LLM_REGEXES"
+    echo "### Testing $regex ###" >> "$GIT_RESULTS"
+    echo "$regex" | java -jar ReScue.jar >> "$GIT_RESULTS"
+    echo >> "$GIT_RESULTS"
+done < "$GIT_REGEXES"
